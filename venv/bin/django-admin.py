1431d83f0bdddea3b0801556e9e35513dd121727
Smikhalcv <Smikhalcv@yandex.ru>
2020-08-31 13:14:21 +0300

    orm-mgrations

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/dell-ubuntu/Документы/Python/django/orm_migrations/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
